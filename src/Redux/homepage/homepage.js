989642cba524f4be273e135ce6bafcc71c4e7597
Rayan84 <rayan_84@live.com>
2021-11-24 22:41:59 +0100

    Add forecast URL

--- src/Redux/homepage/homepage.js
+++ src/Redux/homepage/homepage.js
@@ -2,4 +2,5 @@ import React from 'react';
 import store from '../configureStore';
 
 const fetchDataRequest = 
-const CitiesURL = 'http://dataservice.accuweather.com/locations/v1/topcities/{group}';
+const citiesURL = 'http://dataservice.accuweather.com/locations/v1/topcities/{group}';
+const forecastURL = 'https://developer.accuweather.com/accuweather-forecast-api/apis/get/forecasts/v1/daily/5day/%7BlocationKey%7D';
