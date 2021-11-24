8f0f0f8ea9675c5ef4689d6f5da4f0ff12fee111
Rayan84 <rayan_84@live.com>
2021-11-24 22:34:46 +0100

    Add redux

---
+++ src/Redux/homepage/homepage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import store from '../configureStore';
+
+const fetchDataRequest = 
+const CitiesURL = 'http://dataservice.accuweather.com/locations/v1/topcities/{group}';
