4807b88cbeb325fe39c274a1194a70ac8da5bebb
Rayan84 <rayan_84@live.com>
2021-11-25 11:01:50 +0100

    Add logger

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,5 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import homepageReducer from './homepage/homepage';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer, applyMiddleware(logger, reduxThunk));
+const rootReducer =
 
 export default store;
